6cecf81b27c59ac006feeb743b7175390bb1858f
Tristan Havelick <tristan@havelick.com>
2023-12-27 14:13:59 -0700

    fix up crons

--- roles/backup/files/prebackup.sh
+++ roles/backup/files/prebackup.sh
@@ -3,8 +3,11 @@ set -e
 
 backup_staging_directory=$1
 wallabag_db_file=$2
-
 miniflux_dump_file="${backup_staging_directory}/miniflux.sql"
+
+# change to the backup staging directory to avoid problems where the postgres
+# user can't cd to /root
+cd "${backup_staging_directory}"
 sudo -u postgres pg_dump miniflux_db > "${miniflux_dump_file}"
 
 # copy the wallbag db file to the backup staging directory
