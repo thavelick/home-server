083086de1be6656efd36760bef1796a43ee47e1d
Tristan Havelick <tristan@havelick.com>
2024-01-05 15:13:53 -0700

    initial setup of firefly iii financials

--- roles/backup/files/prebackup.sh
+++ roles/backup/files/prebackup.sh
@@ -3,7 +3,9 @@ set -e
 
 backup_staging_directory=$1
 wallabag_db_file=$2
+firefly_db_file=$3
 miniflux_dump_file="${backup_staging_directory}/miniflux.sql"
+
 
 # change to the backup staging directory to avoid problems where the postgres
 # user can't cd to /root
@@ -13,3 +15,5 @@ sudo -u postgres pg_dump miniflux_db > "${miniflux_dump_file}"
 # copy the wallbag db file to the backup staging directory
 cp "${wallabag_db_file}" "${backup_staging_directory}"
 
+# copy the firefly db file to the backup staging directory
+cp "${firefly_db_file}" "${backup_staging_directory}"
